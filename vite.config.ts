1e963a41458738f15a4f8c4dc6bd78de1f442808
MuscleWeb <grajesh2125@gmail.com>
2026-02-25 16:06:33 +0530

    add base to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/hifigym/",  // <--- Add this line to fix GitHub Pages base path
   server: {
     host: "::",
     port: 8080,
